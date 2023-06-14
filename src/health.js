709c7d0498995a60f73e1a72c98655d3ea46abb0
RuslanNikitin39 <ruslan-nikitin@mail.ru>
2023-06-14 09:32:09 +0500

    npm init

--- src/health.js
+++ src/health.js
@@ -1,11 +1,11 @@
 export function health(person) {
     if (person.health > 100) {
-      return 'Error, health cannot be more than 100';
+        return 'Error, health cannot be more than 100';
     }
     if (person.health > 50) {
-      return 'healthy';
+        return 'healthy';
     } if (person.health <= 50 && person.health > 15) {
-      return 'wounded';
+        return 'wounded';
     }
     return 'critical';
   }
